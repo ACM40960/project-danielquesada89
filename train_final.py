9cc19485e90c3d9bfd1b1e4e948181db627de123
danielquesada89 <daniel.quesada@ucdconnect.ie>
2024-08-09 11:19:32 +0000

    include generalization part of final model

---
+++ train_final.py
@@ -0,0 +1,61 @@
+
+import subprocess
+import sys
+
+# Install ultralytics
+subprocess.check_call([sys.executable, "-m", "pip", "install", "ultralytics"])
+
+import argparse
+import sys
+import os
+import shutil
+import torch
+
+from ultralytics import YOLO
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Hyperparameters
+    
+    parser.add_argument('--epochs',type=int, help='number of training epochs')
+    parser.add_argument('--patience',type=int, help='patience value')
+    parser.add_argument("--batch", type=int, help='batch size')
+    
+    parser.add_argument('--optimizer', type=str, help='optimizer to use')
+    parser.add_argument('--lr0', type=float, help='initial learning rate')
+    parser.add_argument('--lrf', type=float, help='final learning rate')
+    parser.add_argument('--momentum', type=float, help='momentum')
+    parser.add_argument('--weight_decay', type=float, help='optimizer weight decay')
+
+    # SageMaker specific arguments
+    parser.add_argument("--model-dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
+    parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
+    parser.add_argument("--runs-path", type=str, default=os.environ.get("SM_OUTPUT_DATA_DIR"))
+
+   
+    args = parser.parse_args()
+
+    print('---------------Debug injected environment and arguments--------------------')
+    print(sys.argv)
+    print(os.environ)
+    print('---------------End debug----------------------')
+
+  
+    # Train the YOLO model
+    yolo_model = YOLO("yolov8m-seg.pt")
+    yolo_model.train(data=os.path.join(args.train, "data.yaml"), 
+                     batch=args.batch,
+                     epochs=args.epochs, 
+                     optimizer=args.optimizer, 
+                     lr0=args.lr0, 
+                     lrf=args.lrf, 
+                     momentum=args.momentum,
+                     weight_decay=args.weight_decay,
+                     patience=args.patience,
+                     task='segment',
+                     project=args.runs_path,
+                     val = False) # we include this so that it does not perform the validation as it is not needed
+    
+    yolo_model.export()
